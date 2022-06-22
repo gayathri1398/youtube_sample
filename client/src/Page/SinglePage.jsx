d0cf5a8bbf2f8c935536b36e9723891cef279e49
gayathri1398 <gayathrivasu13@gmail.com>
2022-06-22 18:39:26 +0530

    filtering

--- client/src/Page/SinglePage.jsx
+++ client/src/Page/SinglePage.jsx
@@ -12,15 +12,46 @@ import { getVideos } from '../Redux/Reducer/Data/data.action';
 const SinglePage = () => {
   const dispatch = useDispatch();
   const[datas,setDatas] = useState([]);
+  const[value,setValue] =useState();
+ 
+  
+   const handleChange=(e)=>{
+    // setValue(e.target.name=e.target.value);
+    setValue(e);
+    if(value!=""){
+      console.log(value);
+      // return value
+      let name = datas.filter((data)=>data.snippet.title.toLowerCase().includes(value.toLowerCase()));
+      if(!name){
+      return false
+     }
+     setDatas(name)
+    }
+    
+  
+ }
+ console.log(value);
+  
+ const gettingSpecificVideo =()=>{
+   if(value!=""){
+    
+   }
+
+}
+
 
 useEffect(()=>{
-   dispatch(getVideos()).then(data=> setDatas(data.payload.items)
-  )
+ 
+  dispatch(getVideos()).then(data=> setDatas(data.payload.items) )
+  
+  
 },[])
   
   return (
   <>
-        <NavBar/>
+        <NavBar
+         handleChange={handleChange}
+          />
         <div className='lg:pt-16 px-10 grid md:grid-cols-3 lg:grid-cols-4 gap-10'>
           {
             datas.map((data)=>(
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+
+import React,{useState,useEffect} from 'react';
+// import env from "react-dotenv";
 import { useDispatch } from 'react-redux';
+import {BsSearch} from 'react-icons/bs'
 
 import { getSpecificVideo } from '../Redux/Reducer/Data/data.action';
+// import { getVideos } from '../Redux/Reducer/Data/data.action';
 
 
-
-const NavBar = () => {
+const NavBar = ({gettingSpecificVideo,handleChange}) => {
+    const[datas,setDatas] = useState([]);
+    const[settingData,setSettingData]= useState(false)
     const dispatch = useDispatch();
 
-    dispatch(getSpecificVideo()).then(data=>console.log(data))
+
+    const key = "AIzaSyAF-ByPKNvBqma0OD-IB-viyqvF9SGU_BM";
+    const username= "nokia";
+    // console.log(username)
+
+  
+   
+
+
+useEffect(()=>{
+  if(settingData){
+    dispatch(getSpecificVideo(username,key)).then(data=> setDatas(data.payload.items));
+  }
+    
+  
+},[])
+ 
+
+
     return (
         <div className='container mx-auto px-4 py-4'>
           <div className='flex justify-between'>
@@ -18,8 +41,15 @@ const NavBar = () => {
                   className='w-full' />
                <p className='font-bold text-sm md:text-lg'>YOUTUBE</p>
               </div>
-              <div className='border-2 rounded-sm p-1 lg:w-3/5 flex gap-2'>
-                 <input type="search" name="" id="" placeholder='Search' className="w-full focus:outline-none"/>
+              <div className='border-2 rounded-sm p-1 lg:w-3/5 flex gap-2 items-center'>
+                 <input type="search"
+                  name="" 
+                  id=""
+                  placeholder='Search'
+                  className="w-full focus:outline-none"
+                  onChange={(e)=>handleChange(e.target.value)}
+                  />
+                <BsSearch />
               </div>
           </div>
         </div>
